07e6b6447948dcaf0ef22b2c6af972f5b9e0bb7f
Rika Sun <40652126+rikasun@users.noreply.github.com>
2025-03-21 16:57:35 -0400

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,22 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+}
+variable "instance_type" {
+  description = "The type of EC2 instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+variable "key_name" {
+  description = "The name of the key pair to use for SSH access"
+  type        = string
+}
+
 # Adding a new resource with violation in variables.tf
 resource "aws_config_config_rule" "compliance_rules" {
   name = "compliance-check"
